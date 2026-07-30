e9b4d3ba7b596ada5220a24adae2fc503aa1f5b5
K. SAI PRAVALLIKA <pravs206@gmail.com>
2026-07-30 23:06:46 +0530

    Move Firebase config to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHl-fIpVytDSliY3RPNNi3cr21_mSL5gQ",
-  authDomain: "electrona-be.firebaseapp.com",
-  projectId: "electrona-be",
-  storageBucket: "electrona-be.firebasestorage.app",
-  messagingSenderId: "24124679508",
-  appId: "1:24124679508:web:14d5b31dcda233dbe2d4c1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const googleProvider = new GoogleAuthProvider();
+
+export default app;
